eb0162c878f5c37fe3492fe7dd187ae602fa8561
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2023-10-15 23:11:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css';  // Make sure to save the CSS in a file named "Calculator.css"
 
-function App() {
+const Calculator = () => {
+  const [input, setInput] = useState('');
+
+  const handleClick = (value) => {
+    setInput(prev => prev + value);
+  };
+
+  const handleEvaluate = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch (error) {
+      setInput("Error");
+    }
+  };
+
+  const handleClear = () => {
+    setInput('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display">{input}</div>
+      <div className="buttons">
+        {[7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'].map((num, index) => (
+          <button 
+            key={index}
+            className="num-button"
+            onClick={() => handleClick(num)}
+          >
+            {num}
+          </button>
+        ))}
+        {['+', '-', '*', '/', '='].map((op, index) => (
+          <button 
+            key={index} 
+            className={op === '=' ? "equal-button" : "op-button"}
+            onClick={() => op === '=' ? handleEvaluate() : handleClick(op)}
+          >
+            {op}
+          </button>
+        ))}
+        <button className="clear-button" onClick={handleClear}>Clear</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;;
